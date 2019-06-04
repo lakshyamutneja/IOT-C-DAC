ef5f2cd6e04bd5c0acf5bd2fdd558c9ec951e494
Lakshya <mutnejalakshya98@gmail.com>
2019-06-04 11:10:47 +0530

    Blink Sample red

---
+++ Blink_sample/Blink_sample.ino
@@ -0,0 +1,31 @@
+/*
+  Blink
+  The basic Energia example.
+  Turns on an LED on for one second, then off for one second, repeatedly.
+  Change the LED define to blink other LEDs.
+  
+  Hardware Required:
+  * LaunchPad with an LED
+  
+  This example code is in the public domain.
+*/
+
+// most launchpads have a red LED
+#define LED RED_LED
+
+//see pins_energia.h for more LED definitions
+//#define LED GREEN_LED
+  
+// the setup routine runs once when you press reset:
+void setup() {                
+  // initialize the digital pin as an output.
+  pinMode(78, OUTPUT);     
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  digitalWrite(78, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);               // wait for a second
+  digitalWrite(78, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);               // wait for a second
+}
